e1805bdde6897b4b15d17f79bf760c232fca07c7
jzhou45 <jonathanzhou77@gmail.com>
2022-08-13 00:25:50 -0400

    built users table

--- db/migrate/20220812215013_create_users.rb
+++ db/migrate/20220812215013_create_users.rb
@@ -1,8 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :birthday, null: false
+      t.string :gender, null: false
+      t.text :about_me
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
       t.timestamps
     end
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
