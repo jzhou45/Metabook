6978dae838c943479fa712bb14dd0d5f837d24b5
jzhou45 <jonathanzhou77@gmail.com>
2022-08-14 18:18:19 -0400

    added jquery to application.js

--- frontend/metabook.jsx
+++ frontend/metabook.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as SessionUtil from "./util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to Metabook</h1>, root)
+
+    window.login = SessionUtil.login;
+    window.logout = SessionUtil.logout;
+    window.signup = SessionUtil.signup;
+
+    ReactDOM.render(<h1>Welcome to Metabook</h1>, root);
 })
