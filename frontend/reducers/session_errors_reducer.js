38c7d25c63fde6eafbdad7f2df3be2b77f7b202f
jzhou45 <jonathanzhou77@gmail.com>
2022-08-14 19:20:02 -0400

    fixed session errors reducer typos

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
   
-const sessionErrorReducer = (state = [], action) => {
+const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_SESSION_ERRORS:
@@ -12,4 +12,4 @@ const sessionErrorReducer = (state = [], action) => {
     }
 };
 
-export default sessionErrorReducer;
+export default sessionErrorsReducer;
