c7e6e0ec5da43d034a1548ff1b74361ff387b293
jzhou45 <jonathanzhou77@gmail.com>
2022-08-14 19:07:24 -0400

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
     end
 
     def self.find_by_credentials(email, password)
-        user = User.find_by_credentials(email: email)
+        user = User.find_by(email: email)
         return nil unless user
         user.is_password?(password) ? user:nil
     end
--- frontend/metabook.jsx
+++ frontend/metabook.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as SessionUtil from "./util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-
-    window.login = SessionUtil.login;
-    window.logout = SessionUtil.logout;
-    window.signup = SessionUtil.signup;
 
     ReactDOM.render(<h1>Welcome to Metabook</h1>, root);
 })
