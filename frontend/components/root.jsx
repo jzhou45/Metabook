ca167b4fe287b26334872cd24c3035455fda4706
jzhou45 <jonathanzhou77@gmail.com>
2022-08-15 18:22:18 -0400

    pushing to heroku

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-import App from "./app";
+import Test from "./test";
+// import App from "./App";
+
+
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-        <App />
+        <Test />
     </HashRouter>
   </Provider>
 );
