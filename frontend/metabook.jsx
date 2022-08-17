cd90da93c3b04e16b76f74175ea76ce291631723
jzhou45 <jonathanzhou77@gmail.com>
2022-08-17 16:53:10 -0400

    user now stays logged in

--- frontend/metabook.jsx
+++ frontend/metabook.jsx
@@ -4,24 +4,23 @@ import configureStore from "./store/store";
 import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
-    
-    
     let store;
     if (window.currentUser) {
-        const preloadedState = {
-            entities: {
-                users: {[window.currentUser.id]: window.currentUser}
-            },
-            session: {id: window.currentUser.id}
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
+    const preloadedState = {
+        entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+        },
+        session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
     } else {
-        store = configureStore()
+    store = configureStore();
     }
+
+    const root = document.getElementById('root')
+
     window.store = store;
-    
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     
