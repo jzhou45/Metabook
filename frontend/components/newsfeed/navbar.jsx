6a7b09b5590807af6b9f00723e3c3655e503df22
jzhou45 <jonathanzhou77@gmail.com>
2022-08-17 17:00:55 -0400

    added elements to navbar

--- frontend/components/newsfeed/navbar.jsx
+++ frontend/components/newsfeed/navbar.jsx
@@ -12,7 +12,15 @@ class NavBar extends React.Component{
 
     render(){
         return(
-            <button onClick={this.logout}>Logout</button>
+            <header id="navbar">
+                <h1>metabook</h1>
+                <input type="text" id="search-bar"/>
+                <div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png" alt="linkedin link" />
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github link" />
+                    <button onClick={this.logout}>Logout</button>
+                </div>
+            </header>
         )
     }
 }
