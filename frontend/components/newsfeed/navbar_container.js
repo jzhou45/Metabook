d17cfe33a13ab316c2ea1df406ea7e196a16880c
jzhou45 <jonathanzhou77@gmail.com>
2022-08-17 16:56:47 -0400

    fixed logout button import

--- frontend/components/newsfeed/navbar_container.js
+++ frontend/components/newsfeed/navbar_container.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import NavBar from "./navbar";
-import { logout } from "../../util/session_api_util";
+import {logout} from "../../actions/session_actions";
 
 const mapStateToProps = ({session, entities: {users}}) => {
     return{
