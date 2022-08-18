00e385b0ee1c1be46d201f5de409f72999ef4b5c
jzhou45 <jonathanzhou77@gmail.com>
2022-08-17 21:55:05 -0400

    added routes to header links

--- frontend/components/newsfeed/navbar.jsx
+++ frontend/components/newsfeed/navbar.jsx
@@ -11,14 +11,22 @@ class NavBar extends React.Component{
         this.props.logout();
     }
 
+    openLinkedIn(){
+        window.open("https://www.linkedin.com/in/jonathanzhou77/");
+    }
+
+    openGithub(){
+        window.open("https://github.com/jzhou45/Metabook");
+    }
+
     render(){
         return(
             <header id="navbar">
                 <h1>metabook</h1>
                 <input type="text" id="search-bar" placeholder="Search Metabook"/>
                 <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png" alt="linkedin link" />
-                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github link" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png" alt="linkedin link" onClick={this.openLinkedIn} />
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github link" onClick={this.openGithub}/>
                     <button onClick={this.logout}>Logout</button>
                 </div>
             </header>
