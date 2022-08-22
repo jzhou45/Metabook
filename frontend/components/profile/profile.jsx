af5956fcd9e24b68b5000584a82c545862a59b0e
jzhou45 <jonathanzhou77@gmail.com>
2022-08-22 10:14:30 -0400

    fixed user image issues

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -3,12 +3,31 @@ import React from "react";
 class Profile extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            id: null,
+            aboutMe: null,
+            email: null,
+            coverPhoto: null,
+            profilePhoto: null,
+            firstName: null,
+            lastName: null
+        };
     }
 
     componentDidMount(){
         this.props.fetchUser(this.props.usersId).then(
-            () => {
-                document.getElementById("user-name").innerHTML = this.props.users.first_name + " " + this.props.users.last_name;
+            (user) => {
+                this.setState({
+                    id: user.user.id,
+                    email: user.user.email,
+                    aboutMe: user.user.about_me,
+                    firstName: user.user.first_name,
+                    lastName: user.user.last_name,
+                    profilePhoto: user.user.profilePhoto,
+                    coverPhoto: user.user.coverPhoto
+                })
+                console.log(this.state)
+                document.getElementById("user-name").innerHTML = this.state.firstName + " " + this.state.lastName;
             })
     }
 
@@ -17,10 +36,10 @@ class Profile extends React.Component{
             <div>
                 <div id="profile-header">
                     <div id="cover-photo">
-                        <img src={this.props.users.coverPhoto} alt="cover photo" />
+                        <img src={this.state.coverPhoto} alt="cover photo" />
                     </div>
                     <div id="profile-photo">
-                        <img src={this.props.users.profilePhoto} alt="profile photo" />
+                        <img src={this.state.profilePhoto} alt="profile photo" />
                         <h1 id="user-name"></h1>
                     </div>
                 </div>
