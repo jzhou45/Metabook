517671c4cb267979d4ad70f0f75ce881db883663
jzhou45 <jonathanzhou77@gmail.com>
2022-08-21 21:34:44 -0400

    users now get own cover photo and profile photo

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! user, :id, :email, :first_name, :last_name, :about_me
-json.profilePhoto = url_for(user.profile_photo)
-json.coverPhoto = url_for(user.cover_photo)
+json.profilePhoto url_for(user.profile_photo)
+json.coverPhoto url_for(user.cover_photo)
--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -17,10 +17,10 @@ class Profile extends React.Component{
             <div>
                 <div id="profile-header">
                     <div id="cover-photo">
-                        <img src="https://swall.teahub.io/photos/small/51-512779_dream-imagine-believe-facebook-cover.jpg" alt="cover photo" />
+                        <img src={this.props.users.coverPhoto} alt="cover photo" />
                     </div>
                     <div id="profile-photo">
-                        <img src="https://s167.daydaynews.cc/?url=http%3A%2F%2Fp1.pstatp.com%2Flarge%2Fpgc-image%2F82530511de2a45dfadee9614bb407187" alt="profile photo" />
+                        <img src={this.props.users.profilePhoto} alt="profile photo" />
                         <h1 id="user-name"></h1>
                     </div>
                 </div>
