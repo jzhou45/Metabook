91d5f06c48aab3eab2d51135c9688193435462a3
jzhou45 <jonathanzhou77@gmail.com>
2022-08-23 13:37:11 -0400

    added post actions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,22 @@
+import * as APIUtil from "../util/posts_api_utils";
+
+export const RECEIVE_ALL_POSTS = "RECEIVE_ALL_POSTS";
+export const RECEIVE_POST = "RECEIVE_POST";
+
+const receiveAllPosts = (posts) => ({
+    type: RECEIVE_ALL_POSTS,
+    posts
+});
+
+const receivePost = post => ({
+    type: RECEIVE_POST,
+    post
+})
+
+export const fetchPosts = () => dispatch => (
+    APIUtil.fetchPosts().then((posts) => dispatch(receiveAllPosts(posts)))
+);
+
+export const fetchPost = postId => dispatch => (
+    APIUtil.fetchPost(postId).then(post => dispatch(receivePost(post)))
+);
