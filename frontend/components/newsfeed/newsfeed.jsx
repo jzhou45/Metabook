0983fcfbcf625d2247f01ba79e300a9c97307018
jzhou45 <jonathanzhou77@gmail.com>
2022-09-05 16:26:52 -0400

    refactor newsfeed and css

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -1,122 +1,121 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostItem from "./post_item";
+import { Link } from "react-router-dom";
+import { fetchPosts } from "../../actions/post_actions";
+import { openModal } from "../../actions/modal_actions";
+import { connect } from "react-redux";
 
-class Newsfeed extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            content: "",
-            userId: this.props.userId,
-            posts: {},
-            rerender: 0
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleUpdate = this.handleUpdate.bind(this);
-        this.goToProfilePage = this.goToProfilePage.bind(this);
-        this.modalControls = this.modalControls.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-        this.openModal = this.openModal.bind(this);
-        this.rerenderParentCallback = this.rerenderParentCallback.bind(this);
-    };
+//     handleSubmit(e){
+//         e.preventDefault();
+//         const formData = new FormData();
+//         formData.append('post[content]', this.state.content);
+//         formData.append('post[user_id]', this.state.userId);
+//         if (e.target[1].files[0]) formData.append('post[photo]', e.target[1].files[0]);
+//         $.ajax({
+//             url: 'api/posts',
+//             method: "POST",
+//             data: formData,
+//             contentType: false,
+//             processData: false
+//         }).then(() => {
+//             this.setState({
+//                 content: ""
+//             });
+//             this.props.fetchPosts().then(posts => {
+//                 this.setState({posts: posts.posts})
+//             });
+//         });
+//         this.closeModal();
+//     };
+//                 {/* <div id="gray-screen" className="invisible" onClick={this.closeModal}></div>
+//                 <div id="posts-modal" className="invisible">
+//                     <div className="create-post">
+//                         <div></div>
+//                         <h1>Create post</h1>
+//                         <div onClick={this.closeModal}>X</div>
+//                     </div>
+//                     <div>
+//                         <img src={this.props.profilePhoto} alt="profile photo" onClick={this.goToProfilePage}/>
+//                         <p onClick={this.goToProfilePage}>{this.props.firstName} {this.props.lastName}</p>
+//                     </div>
+//                     <form onSubmit={this.handleSubmit}>
+//                         <textarea value={this.state.content} onChange={this.handleUpdate('content')} placeholder={`What's on your mind, ${this.props.firstName}?`}></textarea>
+//                         <label htmlFor="create-post-file">Add image to your post</label>
+//                         <input type="file" onChange={e => {this.setState({file: e.target.files})}} className="invisible" id="create-post-file"/>
+//                         <button type="submit">Post</button>
+//                     </form> */}
+//                 {/* </div> */}
 
-    componentDidMount(){
-        this.props.fetchPosts().then(posts => {
-            this.setState({posts: posts.posts})
-        });
-    };
+const Newsfeed = props => {
+    const {userId, profilePhoto, firstName, fetchPosts, openModal} = props;
 
-    handleUpdate(field){
-        return e => this.setState({[field]: e.currentTarget.value});
-    };
+    const [state, setState] = useState({
+        content: "",
+        userId: userId,
+        posts: {},
+        rerender: 0
+    });
 
-    handleSubmit(e){
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('post[content]', this.state.content);
-        formData.append('post[user_id]', this.state.userId);
-        if (e.target[1].files[0]) formData.append('post[photo]', e.target[1].files[0]);
-        $.ajax({
-            url: 'api/posts',
-            method: "POST",
-            data: formData,
-            contentType: false,
-            processData: false
-        }).then(() => {
-            this.setState({
-                content: ""
+    useEffect(() => {
+        console.log("DEODJ")
+        fetchPosts()
+            .then(posts => {
+                setState({
+                    ...state,
+                    posts: posts.posts
+                });
             });
-            this.props.fetchPosts().then(posts => {
-                this.setState({posts: posts.posts})
+    }, []);
+
+    const rerenderNewsfeed = () => {
+        fetchPosts().then(posts => {
+            setState({
+                ...state,
+                posts: posts.posts
             });
         });
-        this.closeModal();
     };
 
-    goToProfilePage(){
-        this.props.history.push(`/users/${this.props.userId}`);
-    };
-
-    modalControls(){
-        (this.props.modal.modal) ? (this.closeModal()) : (this.openModal());
-    }
-
-    openModal(){
-        document.getElementById("posts-modal").classList.remove("invisible");
-        document.getElementById("posts-modal").classList.add("flex");
-        document.getElementById("gray-screen").classList.remove("invisible");
-        document.body.style.overflow = "hidden";
-        this.props.openModal("posts-modal");
-    };
-
-    closeModal(){
-        document.getElementById("posts-modal").classList.add("invisible");
-        document.getElementById("posts-modal").classList.remove("flex");
-        document.getElementById("gray-screen").classList.add("invisible");
-        document.body.style.overflow = "scroll";
-        this.props.closeModal();
-    }
-
-    rerenderParentCallback(){
-        this.props.fetchPosts().then(posts => {
-            this.setState({posts: posts.posts})
-        });
-    };
-
-    render(){
-        return(
-            <div id="newsfeed">
-                <div id="make-posts">
-                    <img src={this.props.profilePhoto} alt="profile photo" onClick={this.goToProfilePage}/>
-                    <input type="text" placeholder={`What's on your mind, ${this.props.firstName}?`} onClick={this.modalControls} value={this.state.content} />
-                </div>
-
-                <div id="gray-screen" className="invisible" onClick={this.closeModal}></div>
-                <div id="posts-modal" className="invisible">
-                    <div className="create-post">
-                        <div></div>
-                        <h1>Create post</h1>
-                        <div onClick={this.closeModal}>X</div>
-                    </div>
-                    <div>
-                        <img src={this.props.profilePhoto} alt="profile photo" onClick={this.goToProfilePage}/>
-                        <p onClick={this.goToProfilePage}>{this.props.firstName} {this.props.lastName}</p>
-                    </div>
-                    <form onSubmit={this.handleSubmit}>
-                        <textarea value={this.state.content} onChange={this.handleUpdate('content')} placeholder={`What's on your mind, ${this.props.firstName}?`}></textarea>
-                        <label htmlFor="create-post-file">Add image to your post</label>
-                        <input type="file" onChange={e => {this.setState({file: e.target.files})}} className="invisible" id="create-post-file"/>
-                        <button type="submit">Post</button>
-                    </form>
-                </div>
-
-                <div id="all-posts">
-                    {Object.values(this.state.posts).map(post => (
-                        <PostItem post={post} key={post.id} fetchUser={this.props.fetchUser} history={this.props.history} editPost={this.props.editPost} fetchPost={this.props.fetchPost} rerenderParentCallback={this.rerenderParentCallback} currentUser={this.props.userId}/>
-                    ))}
-                </div>
+    return(
+        <div className="newsfeed">
+            <div className="make-posts">
+                <Link to={`/users/${userId}`}>
+                    <img src={profilePhoto} alt="profile photo" />
+                </Link>
+                <input 
+                    type="text" 
+                    placeholder={`What's on your mind, ${firstName}?`}
+                    onClick={() => openModal("makePosts")}
+                    readOnly={state.content}
+                />
             </div>
-        );
-    };
+
+            <div className="all-posts">
+                {Object.values(state.posts).map(post => (
+                    <PostItem
+                        post={post}
+                        key={post.id}
+                        rerenderNewsfeed={rerenderNewsfeed}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
-export default Newsfeed;
+const mapStateToProps = state => {
+    const userId = state.session.id;
+    const currentUserInfo = state.entities.users[userId]
+    return({
+        userId,
+        profilePhoto: currentUserInfo.profilePhoto,
+        firstName: currentUserInfo.firstName
+    });
+};
+
+const mapDispatchToProps = dispatch => ({
+    fetchPosts: () => dispatch(fetchPosts()),
+    openModal: modal => dispatch(openModal(modal))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Newsfeed);
