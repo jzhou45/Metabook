3f20fef0b83b741f9d13fcb4a407900d80eb2839
jzhou45 <jonathanzhou77@gmail.com>
2022-09-05 01:01:29 -0400

    refractor login errors

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -4,43 +4,6 @@ import { clearErrors, login } from "../../actions/session_actions";
 import { openModal } from "../../actions/modal_actions";
 
 // class LoginForm extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             email: '',
-//             password: '',
-//             loggedIn: false,
-//             first_name: '',
-//             last_name: '',
-//             birthday: '',
-//             gender: ''
-//         };
-//         document.getElementsByTagName("html")[0].style.backgroundColor = "#eff2f5";
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//         this.handleUpdate = this.handleUpdate.bind(this);
-//         this.logout = this.logout.bind(this);
-//         this.openModal = this.openModal.bind(this);
-//         this.loginAsDemoUser = this.loginAsDemoUser.bind(this);
-//         this.handleErrors = this.handleErrors.bind(this);
-//         this.clearErrors = this.clearErrors.bind(this);
-//     };
-
-//     handleUpdate(field){
-//         return e => this.setState({[field]: e.currentTarget.value});
-//     };
-
-//     handleSubmit(e){
-//         e.preventDefault();
-//         this.props.login(this.state);
-//         if (this.props.sessionid) this.setState({loggedIn: true});
-//         this.handleErrors();
-//     };
-
-//     logout(){
-//         this.props.logout();
-//         this.setState({loggedIn: false});
-//     };
-
 //     openModal(){
 //         this.props.openModal("modal");
 //         const modal = document.getElementById("modal");
@@ -52,19 +15,6 @@ import { openModal } from "../../actions/modal_actions";
 //         this.clearErrors();
 //     };
 
-//     clearErrors(){
-//         this.props.clearSessionErrors()
-//         document.getElementById("login-email").style.border = "0.5px solid #dde0e3";
-//         document.getElementById("login-email-error-message").innerHTML = "";
-//         document.getElementById("login-container").firstChild.style.height = "390px";
-//         document.getElementById("login-password").style.border = "0.5px solid #dde0e3";
-//         document.getElementById("login-password-error-message").innerHTML = "";
-//     };
-
-//     loginAsDemoUser(){
-//         this.props.login({email:"demouser@email.com", password: "password"});
-//         this.setState({loggedIn:true});
-//     };
 
 //     handleErrors(){
 //         if (this.state.email.length === 0){
@@ -73,50 +23,6 @@ import { openModal } from "../../actions/modal_actions";
 //             document.getElementById("login-container").firstChild.style.height = "420px";
 //         };
 //         if (this.state.password.length === 0){
-//             document.getElementById("login-password").style.border = "1px solid #f02849";
-//             document.getElementById("login-password-error-message").innerHTML = "The password you've entered is incorrect";
-//             document.getElementById("login-container").firstChild.style.height = "420px";
-//         };
-//     };
-
-//     render(){
-//         if (!this.props.sessionid){
-//             return(
-//                 <div id="login-signup-forms" className="invisible">
-//                     <div id="white-background"></div>
-
-//                     <div id="greetings">
-//                         <h1>metabook</h1>
-//                         <h2>Connect with friends and the world around you on Metabook.</h2>
-//                     </div>
-
-//                     <div id="login-container">
-//                         <form onSubmit={this.handleSubmit}>
-//                             <input type="text" name="email" value={this.state.email} onChange={this.handleUpdate('email')} id="login-email" placeholder="Email" onFocus={this.clearErrors}/>
-//                             <p id="login-email-error-message" className="login-errors"></p>
-//                             <input type="password" name="password" value={this.state.password} onChange={this.handleUpdate('password')} id="login-password" placeholder="Password" onFocus={this.clearErrors}/>
-//                             <p id="login-password-error-message" className="login-errors"></p>
-
-//                             <button type="submit" className="login-button">Log In</button>
-
-//                             <div id="demo-login" onClick={this.loginAsDemoUser}>Login as Demo User?</div>
-
-//                             <hr />
-//                             <div onClick={this.openModal} className="signup-button">
-//                                 <p>Create new account</p>
-//                             </div>
-//                         </form>
-//                         <div className="create-a-page">
-//                             <p><span>Create a Page </span> for a celebrity, brand or business.</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             );
-//         };
-//     };
-// };
-// 
-// export default LoginForm;
 
 const LoginForm = props => {
     const { errors, login, openModal, clearErrors} = props;
@@ -145,6 +51,8 @@ const LoginForm = props => {
         });
     };
 
+    const loginError = errors[0] ? "login-input-errors" : null;
+
     return(
         <div className="login-signup-forms">
             <div className="greetings">
@@ -154,12 +62,13 @@ const LoginForm = props => {
 
             <div className="login-container">
                 <form onSubmit={handleSubmit} className="login-form">
-                <input
+                    <input
                         type="text"
                         name="email"
                         value={state.email}
                         onChange={handleUpdate("email")}
                         placeholder="Email"
+                        className={loginError}
                     />
 
                     <input
@@ -168,7 +77,10 @@ const LoginForm = props => {
                         value={state.password}
                         onChange={handleUpdate("password")}
                         placeholder="Password"
+                        className={loginError}
                     />
+
+                    {(errors) ? (<p className="login-errors">{errors[0]}.</p>) : null}
 
                     <button type="submit" className="login-button">Log In</button>
 
