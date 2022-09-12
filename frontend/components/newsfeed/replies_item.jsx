56faa638c8ec37a5b2a54e1bbac0990e748d9dc7
jzhou45 <jonathanzhou77@gmail.com>
2022-09-12 15:59:27 -0400

    add edit function to replies

--- frontend/components/newsfeed/replies_item.jsx
+++ frontend/components/newsfeed/replies_item.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Reply = props => {
-    const {reply, fetchUser, replierId} = props
+    const {reply, fetchUser, replierId, commentObj, rerenderParent} = props
 
     const [state, setState] = useState({
         profilePhoto: "",
         firstName: "",
-        lastName: ""
+        lastName: "",
+        dropdown: false,
+        editing: false,
+        comment: reply,
+        previousComment: reply
     });
 
     useEffect(() => {
@@ -20,6 +24,46 @@ const Reply = props => {
             });
         });
     }, []);
+
+    const handleClick = () => {
+        (state.dropdown) ? (setState({...state, dropdown: false})) : (setState({...state, dropdown: true}));
+    };
+
+    const handleEdit = () => {
+        setState({
+            ...state,
+            dropdown: false,
+            editing: true
+        });
+    };
+
+    const handleSubmitEdit = e => {
+        e.preventDefault();
+        const commentData = new FormData();
+        commentData.append("comment[comment]", state.comment);
+
+        $.ajax({
+            method: "PATCH",
+            url: `api/comments/${commentObj.id}`,
+            data: commentData,
+            contentType: false,
+            processData: false
+        }).then(() => {
+            setState({
+                ...state,
+                editing: false
+            });
+            
+            rerenderParent();
+        });
+    };
+
+    const handleUpdate = field => (
+        e => setState({
+            ...state,
+            [field]: e.currentTarget.value
+        })
+    );
 
     return(
         <div className="reply">
@@ -36,16 +80,34 @@ const Reply = props => {
             <div className="reply-align">
                 <div className="reply-body">
                     <Link to={`users/${replierId}`}>{state.firstName} {state.lastName}</Link>
-                    {reply}
+
+                    {(state.editing) ?
+                    
+                    (<form onSubmit={handleSubmitEdit}>
+                        <input type="text" 
+                            value={state.comment} 
+                            onChange={handleUpdate("comment")}
+                            autoFocus
+                        />
+                    </form>) : 
+                    reply}
+
                 </div>
 
-                <div className="comments-dropdown-div">
+                <div className="comments-dropdown-div" onClick={handleClick}>
                     <img 
                         src="https://miro.medium.com/max/512/1*Js0Y20MwjcTnVAe7KjDXNg.png" 
                         alt="open comment dropdown" 
                         className="comments-dropdown"
                         />
                 </div>
+
+                {(state.dropdown) ?
+                (<div className="reply-dropdown-menu">
+                    <span onClick={handleEdit}>Edit</span>
+                    <span>Delete</span>
+                </div>) :
+                null}
             </div>
         </div>
     );
